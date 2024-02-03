6cc17fe94d684232dec8b9848f8f6bae1be127d9
Jangmin O <jangmin.o@gmail.com>
2024-02-03 14:58:50 +0900

    initialview

---
+++ examples/whisper.objc/whisper.objc/InitialViewController.h
@@ -0,0 +1,16 @@
+//
+//  InitialViewController.h
+//  my-order-genie
+//
+//  Created by 오장민 on 2/3/24.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InitialViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ examples/whisper.objc/whisper.objc/InitialViewController.m
@@ -0,0 +1,31 @@
+//
+//  InitialViewController.m
+//  my-order-genie
+//
+//  Created by 오장민 on 2/3/24.
+//
+
+#import "InitialViewController.h"
+
+@interface InitialViewController ()
+
+@end
+
+@implementation InitialViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
